66c8936e02a058b88732fa4a2f57c5fffe4d5137
Kat <neridova13@gmail.com>
2019-11-04 17:41:27 -0500

    pony added

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/pony', function (req, res) {
+    res.sendFile(`${__dirname}/public/index.html`)
+  })
  
-app.listen(3000)
+app.listen(4000, '0.0.0.0', ()=>{
+    console.warn('Server express start at http://0.0.0.0:4000/')
+})
